3c5030af6688d0d068bab0e1d2a7bde90244be57
apalski <annette.drapalski@gmail.com>
2018-01-23 09:05:28 +1100

    Refactor update quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,56 +4,76 @@ class GildedRose
     @items = items
   end
 
+  def past_sell_in_date?(item)
+    item.sell_in < 0
+  end
+
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+      if item.valid_quality? && !past_sell_in_date?(item)
+        if item.special_item?
+          item.increase_quality
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
+          item.decrease_quality
+        end
+
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          item.quality += 2
+          if item.sell_in < 11
+            item.increase_quality
           end
+          if item.sell_in < 6
+            item.increase_quality
+          end
+        end
+
+        if item.name != "Sulfuras, Hand of Ragnaros"
+          item.sell_in -= 1
+        end
+      end
+
+      if item.valid_quality? && past_sell_in_date?(item)
+        if item.special_item?
+          item.increase_quality
+        elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+          item.quality = 0
+        else
+          item.decrease_quality
         end
       end
     end
   end
 end
 
-Item = Struct.new(:name, :sell_in, :quality)
+class Item
+
+  attr_reader :name
+  attr_accessor :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def special_item?
+    ["Aged Brie",
+      "Sulfuras, Hand of Ragnaros",
+    ].include?(name)
+  end
+
+  def valid_quality?
+    (0...50).include?(quality)
+  end
+
+  def increase_quality
+    self.quality += 1
+  end
+
+  def decrease_quality
+    self.quality -= 1
+  end
+end
 # Items = [
 #   Item.new("+5 Dexterity Vest", 10, 20),
 #   Item.new("Aged Brie", 2, 0),
